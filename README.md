6a566d3b0f98af436fcb1670718c33e0cc6dae99
zhangkaiqiang <zhangkaiqiang@58ganji.com>
2017-07-07 19:21:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# vue-cli-mult
+
+> 基于vue-cli的 VUE 多页面构建手脚架
+
+## Build Setup
+
+```bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev -- xx
+
+# build for production with minification
+npm run build -- xx
+```
---
+++ build/webpack.base.conf.js
@@ -0,0 +1,87 @@
+var path = require('path')
+var webpack = require('webpack')
+var utils = require('./utils')
+var config = require('../config')
+var os = require('os')
+var HappyPack = require('happypack')
+var happThreadPool = HappyPack.ThreadPool({
+  size: os.cpus().length
+})
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+module.exports = {
+  entry: {
+    app: './src/module/' + config.project + '/main.js'
+  },
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production' ?
+      config.build.assetsPublicPath : config.dev.assetsPublicPath
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': resolve('src')
+    }
+  },
+  module: {
+    rules: [{
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [resolve('src'), resolve('test')],
+        options: {
+          formatter: require('eslint-friendly-formatter')
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            js: 'happypack/loader?id=js'
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: ['happypack/loader?id=js'],
+        include: [resolve('src'), resolve('test')],
+        exclude: [path.resolve('../../node_modules')]
+      },
+
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HappyPack({
+      id: 'js',
+      cache: true,
+      loaders: ['babel-loader?cacheDirectory=true'],
+      threadPool: happThreadPool
+    }),
+    new webpack.DllReferencePlugin({
+      context: __dirname,
+      manifest: require('../lib/vendor-mainfest.json') // 指向这个json
+    })
+  ]
+}
